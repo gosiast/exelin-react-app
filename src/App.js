bc73513d1fdc97a0a2179f62310530c257d31be8
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-09-07 10:51:08 +0200

    moved pages to the 'pages' folder
    good practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import HomePage from "./HomePage";
-import AnotherPage from "./AnotherPage";
+import HomePage from "./pages/HomePage";
+import AnotherPage from "./pages/AnotherPage";
 export default function App() {
 	return (
 		<Routes>
